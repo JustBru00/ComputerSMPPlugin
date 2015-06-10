ff87c49a5c80127453a53cfe789becb36b78cfb8
JustBru00 <justbru00@gmail.com>
2015-06-10 14:21:15 -0400

    Continuing Making plugin.

--- src/com/gmail/justbru00/computersmp/custom/enchants/gui/main/Main.java
+++ src/com/gmail/justbru00/computersmp/custom/enchants/gui/main/Main.java
@@ -3,20 +3,34 @@ package com.gmail.justbru00.computersmp.custom.enchants.gui.main;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
 	public String Prefix = color("&8[&bEpic&fCustomEnchantsGUI&8]");
+	public FileConfiguration config = getConfig();	
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command,	String label, String[] args) {
 
 		if (command.getName().equalsIgnoreCase("customenchantsgui")) {
-			if (args.length == 1) {
-				
-			} else {
+			if (sender.hasPermission("customenchantsgui.customenchantsgui")) {				
+				if (args.length == 1) {
+					if(args[0].equalsIgnoreCase("version")) {
+						sender.sendMessage(Prefix + color("&fVersion Message Here."));
+					}
+					if(args[0].equalsIgnoreCase("help")) {
+						sender.sendMessage(Prefix + color("&fHelp Message Here."));
+					}else {
+						sender.sendMessage(Prefix + color("&4Please put help or version after /customenchantsgui"));
+					}
+					
+				} else {
 				sender.sendMessage(Prefix + color("&4Please put help or version after /customenchantsgui"));
+				}
+			} else {
+				sender.sendMessage(Prefix + color("&4You don't have permission."));
 			}
 		}
 		
@@ -27,6 +41,10 @@ public class Main extends JavaPlugin {
 		String colored = uncoloredstring.replace('_', ' ');
 		colored = ChatColor.translateAlternateColorCodes('&', colored);
 		return colored;
+	}
+	
+	public String getString(String location){
+		return color(config.getString(location));
 	}
 	
 	@Override
